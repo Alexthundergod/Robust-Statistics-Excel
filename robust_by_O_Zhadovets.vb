63e83966b4511f4f46e1298651ccd32be371b1d0
Oleksandr Zhadovets <100019194+Alexthundergod@users.noreply.github.com>
2024-04-10 23:55:41 +0300

    RSD function added, minor bugs fixed

--- robust_by_O_Zhadovets.vb
+++ robust_by_O_Zhadovets.vb
@@ -1,5 +1,40 @@
-Function MedianAbsDev(ParamArray data_ranges() As Variant) As Single
 'By O.Zhadovets
+'https://github.com/Alexthundergod/Robust-Statistics-Excel
+
+Function RSD(ParamArray data_ranges() As Variant) As Single
+
+Dim totalCells As Long
+totalCells = 0
+
+For Each data_range In data_ranges
+    totalCells = totalCells + data_range.Cells.Count
+Next data_range
+
+Dim dataList() As Single
+ReDim dataList(1 To totalCells)
+Dim currentIndex As Long
+currentIndex = 1
+
+For Each data_range In data_ranges
+    For Each cell In data_range
+        dataList(currentIndex) = cell.Value
+        currentIndex = currentIndex + 1
+    Next cell
+Next data_range
+
+Dim medValue As Single
+medValue = WorksheetFunction.Median(dataList)
+
+Dim j As Long
+Dim resultList() As Single
+ReDim resultList(1 To totalCells)
+For j = 1 To totalCells
+    resultList(j) = Abs(dataList(j) - medValue)
+Next j
+
+RSD = WorksheetFunction.Median(resultList) * 1.4826
+End Function
+Function MedianAbsDev(ParamArray data_ranges() As Variant) As Single
 
 Dim totalCells As Long
 totalCells = 0
@@ -33,7 +68,6 @@ Next j
 MedianAbsDev = WorksheetFunction.Median(resultList)
 End Function
 Function MeanAbsDev(ParamArray data_ranges() As Variant) As Single
-'By O.Zhadovets
 
 Dim totalCells As Long
 totalCells = 0
@@ -67,17 +101,14 @@ Next j
 MeanAbsDev = WorksheetFunction.Sum(resultList) / totalCells
 End Function
 Function PercentageActivity(signal, high_control, low_control As Single) As Single
-'By O.Zhadovets
 
-PercentageActivity = ((signal - low_control) / (high_control - low_control))
+PercentageActivity = ((signal - low_control) / (high_control - low_control)) * 100
 End Function
 Function PercentageInhibition(signal, high_control, low_control As Single) As Single
-'By O.Zhadovets
 
-PercentageInhibition = 1 - ((signal - low_control) / (high_control - low_control))
+PercentageInhibition = (1 - ((signal - low_control) / (high_control - low_control))) * 100
 End Function
 Function RCV(ParamArray data_ranges() As Variant) As Single
-'By O.Zhadovets
 
 Dim totalCells As Long
 totalCells = 0
@@ -111,7 +142,6 @@ Next j
 RCV = WorksheetFunction.Median(resultList) / WorksheetFunction.Median(dataList)
 End Function
 Function CV(ParamArray data_ranges() As Variant) As Single
-'By O.Zhadovets
 
 Dim totalCells As Long
 totalCells = 0
