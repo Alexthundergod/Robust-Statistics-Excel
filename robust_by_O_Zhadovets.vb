4502256e44aba815acbcad42174071ee0a5bc0da
Oleksandr Zhadovets <100019194+Alexthundergod@users.noreply.github.com>
2024-04-16 15:33:16 +0300

    RPercentageDrift & PercentageDrift functions added

--- robust_by_O_Zhadovets.vb
+++ robust_by_O_Zhadovets.vb
@@ -108,6 +108,14 @@ Function PercentageInhibition(signal, high_control, low_control As Single) As Si
 
 PercentageInhibition = (1 - ((signal - low_control) / (high_control - low_control))) * 100
 End Function
+Function RPercentageDrift(data_range1 As Range, data_range2 As Range, plate_range As Range) As Single
+
+RPercentageDrift = (100 * (WorksheetFunction.Median(data_range1) - WorksheetFunction.Median(data_range2))) / WorksheetFunction.Median(plate_range)
+End Function
+Function PercentageDrift(data_range1 As Range, data_range2 As Range, plate_range As Range) As Single
+
+PercentageDrift = (100 * (WorksheetFunction.Average(data_range1) - WorksheetFunction.Average(data_range2))) / WorksheetFunction.Average(plate_range)
+End Function
 Function RCV(ParamArray data_ranges() As Variant) As Single
 
 Dim totalCells As Long
@@ -164,3 +172,15 @@ Next data_range
 
 CV = WorksheetFunction.stDev(dataList) / WorksheetFunction.Average(dataList)
 End Function
+Function ZPrime(high_control_sd, low_control_sd, high_control_mean, low_control_mean As Double) As Double
+
+difference = high_control_mean - low_control_mean
+If difference > 0 Then
+    ZPrime = 1 - (3 * (high_control_sd + low_control_sd) / difference)
+ElseIf difference < 0 Then
+    ZPrime = 1 - (3 * (high_control_sd + low_control_sd) / ((-1) * difference))
+Else
+    ZPrime = 0
+End If
+End Function
+
